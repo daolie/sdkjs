9e909c784cd068f75c39bc556ed81111b6e057e6
Igor Zotov <Igor.Zotov@onlyoffice.com>
2020-11-13 11:15:22 +0300

    [se] By previous

--- cell/model/DataValidation.js
+++ cell/model/DataValidation.js
@@ -643,16 +643,6 @@
 	prot['asc_setFormula1'] = prot.setFormula1;
 	prot['asc_setFormula2'] = prot.setFormula2;
 
-	window['Asc']['EDataValidationType'] = window['Asc'].EDataValidationType = EDataValidationType;
-	prot = EDataValidationType;
-	prot['None'] = prot.None;
-	prot['Date'] = prot.Date;
-	prot['Decimal'] = prot.Decimal;
-	prot['List'] = prot.List;
-	prot['TextLength'] = prot.TextLength;
-	prot['Time'] = prot.Time;
-	prot['Whole'] = prot.Whole;
-
 	window['Asc']['EDataValidationOperator'] = window['Asc'].EDataValidationOperator = EDataValidationOperator;
 	prot = EDataValidationOperator;
 	prot['Between'] = prot.Between;
